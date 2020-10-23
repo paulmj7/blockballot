d33260c74af58dfc7b40ad1a1e7f2f15b21da265
Paul Jones <paulj51299@gmail.com>
2020-10-22 22:31:31 -0400

    building out new blockchain module

--- blockchain/block.ts
+++ blockchain/block.ts
@@ -1,11 +1,25 @@
+import { createHash } from "https://deno.land/std@0.74.0/hash/mod.ts";
+
 export class Block {
   private id: number;
   private transactions: Array<Transaction>;
   private timestamp: Date;
   private previousHash: string;
 
-  constructor(transactions: Array<Transaction>) {
+  constructor(
+    id: number,
+    transactions: Array<Transaction>,
+    timestamp: Date,
+    previousHash: string,
+  ) {
+    this.id = id;
     this.transactions = transactions;
+    this.timestamp = timestamp;
+    this.previousHash = previousHash;
+  }
+
+  computeHash(): string {
+    return createHash("sha256").update(this).toString();
   }
 }
 
--- blockchain/blockchain.ts
+++ blockchain/blockchain.ts
@@ -1,10 +1,29 @@
-import { Block, Transaction } from "./Block";
+import { Block, Transaction } from "./Block.ts";
 
 export class Blockchain {
   private chain: Array<Block>;
   private unconfirmedTransactions: Array<Transaction>;
+  private difficulty: number;
   constructor() {
     this.chain = [];
     this.unconfirmedTransactions = [];
+    this.difficulty = 2;
+    this.init();
+  }
+
+  private init(): void {
+    this.chain.push(new Block(0, [], new Date(Date.now()), "0"));
+  }
+
+  private peek(): Block {
+    return this.chain[this.chain.length - 1];
+  }
+
+  private pushNewBlock(block: Block): void {
+    this.chain.push(block);
+  }
+
+  pushNewTransaction(transaction: Transaction): void {
+    this.unconfirmedTransactions.push(transaction);
   }
 }
